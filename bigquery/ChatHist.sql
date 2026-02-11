76761831a904a04acaffaf3c299f5b438436991c
Pooja Verulkar <50984727+poojanv123@users.noreply.github.com>
2026-02-11 22:56:29 +0530

    Update ChatHist.sql

--- bigquery/ChatHist.sql
+++ bigquery/ChatHist.sql
@@ -3,6 +3,7 @@ config {
   name:"ChatHist",
   dataset:"noora_health_task_whatsapp_chats",
 }
+--join messages and statuses on message_id which is the primary key for messages. Datetime columns are getting read as string in bigquery so convert them to timestamp while joining the tables
 WITH chathistory AS (
 SELECT 
   messages.* EXCEPT (_airbyte_raw_id, _airbyte_extracted_at, _airbyte_meta,_airbyte_generation_id,id, uuid,inserted_at,updated_at),
@@ -20,11 +21,14 @@ SELECT
  FROM `noora_health_task_whatsapp_chats.Messages` AS messages
 LEFT JOIN `noora_health_task_whatsapp_chats.Statuses` AS statuses
   ON messages.id = statuses.message_id),
+--calculate fields required for visualizations
 chathist_with_time_diff as(
 SELECT
   chathistory.*,
+  -- create a flag column with value true if the message has a 'failed' status associated with it
   MAX(status= 'failed')
     OVER (PARTITION BY message_id) AS has_failed,
+  -- calculate the time difference in number of hours between status_inserted_at for read status and sent status for a message
   TIMESTAMP_DIFF(
     MAX(IF(status = 'read', statuses_inserted_at, NULL)) 
       OVER (PARTITION BY message_id),
@@ -35,8 +39,10 @@ SELECT
 FROM chathistory)
 SELECT
   chathist_with_time_diff.* EXCEPT(diff_sent_to_read),
+  -- keep the time difference in sent and read status only for rows with read status and make other rows null for the same message to avoid duplication
+
   CASE
     WHEN status = 'read' THEN diff_sent_to_read
     ELSE NULL
-  END AS hr_to_read_mins
+  END AS hr_to_read
 FROM chathist_with_time_diff

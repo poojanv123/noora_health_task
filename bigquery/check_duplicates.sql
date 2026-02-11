77a038e949f7fea76de234d0bc1a1f1b0c1b6990
Pooja Verulkar <50984727+poojanv123@users.noreply.github.com>
2026-02-11 21:39:33 +0530

    Create check_duplicates.sql

---
+++ bigquery/check_duplicates.sql
@@ -0,0 +1,20 @@
+config {
+  type:"assertion",
+  name:"check_duplicates",
+  dataset:"noora_health_task_whatsapp_chats",
+  dependencyTargets:[{
+    name:"ChatHist"
+  }]
+}
+SELECT
+  a.message_id AS message_id_a,
+  b.message_id AS message_id_b,
+  a.content,
+  a.messages_inserted_at AS ts_a,
+  b.messages_inserted_at AS ts_b,
+  ABS(TIMESTAMP_DIFF(a.messages_inserted_at, b.messages_inserted_at, SECOND)) AS diff_seconds
+FROM `noora_health_task_whatsapp_chats.ChatHist` a
+JOIN `noora_health_task_whatsapp_chats.ChatHist` b
+  ON a.content = b.content
+ where a.message_id != b.message_id
+ AND ABS(TIMESTAMP_DIFF(a.messages_inserted_at, b.messages_inserted_at, SECOND)) < 120
